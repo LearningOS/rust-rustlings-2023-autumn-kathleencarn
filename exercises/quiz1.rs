def60347e75420c4875af6cd9d6fd21430b1c20f
Kathy <92217739+kathleencarn@users.noreply.github.com>
2023-10-10 15:25:33 +0000

    init

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,18 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
+
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(x:i32)->i32{
+    let price = if x<=40{
+        x*2
+    }else {
+        x
+    };
+    price 
+}
+            
 
 // Don't modify this function!
 #[test]
